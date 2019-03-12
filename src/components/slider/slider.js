d294e55e52695b6f244e53a823447739e4886ba3
Lee Neenan <tobycraib@yahoo.com.au>
2019-03-12 21:01:51 +1100

    add handle to slider

--- src/components/slider/slider.js
+++ src/components/slider/slider.js
@@ -1,5 +1,33 @@
 import React, { Component } from 'react'
-import { Slider } from 'react-compound-slider'
+import { Slider, Handles } from 'react-compound-slider'
+
+
+export const Handle = ({handle: {id, value, percent}, getHandleProps}) => {
+    return (
+        <div
+            style={{
+                left: `${percent}%`,
+                position: 'absolute',
+                marginLeft: -15,
+                marginTop: 25,
+                zIndex: 2,
+                width: 30,
+                height: 30,
+                border: 0,
+                textAlign: 'center',
+                cursor: 'pointer',
+                borderRadius: '50%',
+                backgroundColor: '#2C4870',
+                color: '#333',
+            }}
+            {...getHandleProps(id)}
+        >;
+        <div style={{fontFamily:'Roboto', fontSize: 11, marginTop: -35 }}>
+            {value}
+        </div>
+        </div>
+    )
+}
 
 const sliderStyle = {
     position: 'relative',
@@ -22,9 +50,24 @@ export const CoolSlider = () => {
         <Slider
             rootStyle={sliderStyle}
             domain={[0,100]}
-            values={[10]}
+            values={[30]}
+            step={1}
+            mode={2}
         >
             <div style={railStyle} />
+            <Handles>
+                {({ handles, getHandleProps }) => (
+                    <div className="slider-handles">
+                    {handles.map(handle => (
+                        <Handle
+                        key={handle.id}
+                        handle={handle}
+                        getHandleProps={getHandleProps}
+                        />
+                    ))}
+                    </div>
+                )}
+            </Handles>
         </Slider>
     )
 }
